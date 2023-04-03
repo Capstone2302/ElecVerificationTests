7ca18bfe8aee4c77507a76018ffbc23e78452349
seanghaeli <seanghaeli@gmail.com>
2023-04-02 18:52:36 -0700

    stepper_motor

--- Test/stepper_motor.py
+++ Test/stepper_motor.py
@@ -17,20 +17,20 @@ gpio.output(DIR,CW)
 try:
     while True:
         sleep(1)
+        gpio.output(DIR,CCW)
+        for x in range(500):
+            gpio.output(STEP,gpio.HIGH)
+            sleep(.00100)
+            gpio.output(STEP,gpio.LOW)
+            sleep(.00100)
+        
+        sleep(1)
         gpio.output(DIR,CW)
         for x in range(400):
             gpio.output(STEP,gpio.HIGH)
-            sleep(.0100)
+            sleep(.010)
             gpio.output(STEP,gpio.LOW)
-            sleep(.0100)
-        
-        sleep(1)
-        gpio.output(DIR,CCW)
-        for x in range(400):
-            gpio.output(STEP,gpio.HIGH)
-            sleep(.0010)
-            gpio.output(STEP,gpio.LOW)
-            sleep(.0010)
+            sleep(.010)
             
         
 except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
